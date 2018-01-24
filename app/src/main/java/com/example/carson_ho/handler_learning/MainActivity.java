d9766fbc336e96d337e4a5b7216eea8240d07392
Carson.Ho <carson.ho@foxmail.com>
2018-01-24 12:47:48 +0800

    add

--- app/src/main/java/com/example/carson_ho/handler_learning/MainActivity.java
+++ app/src/main/java/com/example/carson_ho/handler_learning/MainActivity.java
@@ -2,74 +2,84 @@ package com.example.carson_ho.handler_learning;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public TextView show;
-    public Handler showhandler;
+
+    public TextView mTextView;
+    public Handler mHandler;
+
+    // 步骤1：（自定义）新创建Handler子类(继承Handler类) & 复写handleMessage（）方法
+    class Mhandler extends Handler {
+
+        // 通过复写handlerMessage() 从而确定更新UI的操作
+        @Override
+        public void handleMessage(Message msg) {
+            // 根据不同线程发送过来的消息，执行不同的UI操作
+            switch (msg.what) {
+                case 1:
+                    mTextView.setText("执行了线程1的UI操作");
+                    break;
+                case 2:
+                    mTextView.setText("执行了线程2的UI操作");
+                    break;
+            }
+        }
+    }
 
     @Override
-    //主线程创建时便自动创建Looper和对应的MessageQueue,之前执行Loop()进入消息循环
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        show = (TextView) findViewById(R.id.show);
-        StringBuffer text = new StringBuffer();
-        text.append("Carson_Ho:Do you love me?");
-        show.setText(text);
-        //实例化Handler,这里并无指定Looper,即自动绑定当前线程(主线程)的Looper和MessageQueue
-        showhandler = new Handler();
-        //启动子线程
-        new Thread_1().start();
-        new Thread_2().start();
+        mTextView = (TextView) findViewById(R.id.show);
 
-    }
+        // 步骤2：创建Handler实例
+        mHandler = new Mhandler();
 
-
-    class Thread_1 extends Thread {
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            showhandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String line = "\n";
-                    StringBuffer text = new StringBuffer(show.getText());
-                    text.append(line).append("angelababy:Yes,I do");
-                    show.setText(text);
+        // 步骤3：在工作线程中 发送消息到消息队列中
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-            });
-        }
-    }
-
-
-
-    class Thread_2 extends Thread {
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(8000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                // 通过sendMessage（）发送
+                // a. 定义要发送的消息
+                Message msg = Message.obtain();
+                msg.what = 1; //消息的标识
+                msg.obj = "A"; // 消息的存放
+                // b. 通过Handler发送消息到其绑定的消息队列
+                mHandler.sendMessage(msg);
             }
-            showhandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    String line = "\n";
-                    StringBuffer text = new StringBuffer(show.getText());
-                    text.append(line).append("黄晓明:what the fuck?");
-                    show.setText(text);
-                }
+        }.start();
+        // 步骤3：开启工作线程（同时启动了Handler）
 
-            });
-        }
+        // 此处用2个工作线程展示
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 通过sendMessage（）发送
+                // a. 定义要发送的消息
+                Message msg = Message.obtain();
+                msg.what = 2; //消息的标识
+                msg.obj = "B"; // 消息的存放
+                // b. 通过Handler发送消息到其绑定的消息队列
+                mHandler.sendMessage(msg);
+            }
+        }.start();
+
     }
+
+
 }
 
